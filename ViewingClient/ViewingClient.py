1ab5efc138073a363f5deebf76174aad0e9fdb61
LAPTOP-7TH3AA3K\Michael <michael.ryan82@mail.dcu.ie>
2019-01-26 15:43:06 +0000

    Added some options

--- ViewingClient/ViewingClient.py
+++ ViewingClient/ViewingClient.py
@@ -4,6 +4,8 @@ HOST = "127.0.0.1"
 PORT = 80
 VIEW_CLIENT =  f"client_type:1".encode()
 
+
+
 def main():
     connection_service = ConnectionService(HOST, PORT, VIEW_CLIENT)
     connection_service.connect(VIEW_CLIENT)
@@ -11,14 +13,23 @@ def main():
                                             # Get available hubs / cameras
     # Request footage from hub + camera                                         # Set motion detect on hub + camera
     # Receive stream and display                                                # Receive push notification on detection
-
-
-
     while True:
         user_input = input("Enter something:")
 
-        if user_input == "g":
-            connection_service.send_message("view:test_hub:1")
+        if user_input == "s":
+            if (connection_service.is_connected()):
+                connection_service.send_message("send:test_hub:1".encode())
+            else:
+                print ("Not connected..")
+
+        if user_input == "x":
+            if (connection_service.is_connected()):
+                connection_service.send_message("stop:test_hub:1".encode())
+            else:
+                print ("Not connected..")
+
+        if user_input == "c":
+            connection_service.connect(VIEW_CLIENT)
 
         if user_input == "quit":
             break
