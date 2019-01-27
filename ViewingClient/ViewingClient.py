f39a5d0a8a966757c0d942f38c161ba47f9bc0b0
LAPTOP-7TH3AA3K\Michael <michael.ryan82@mail.dcu.ie>
2019-01-27 00:02:36 +0000

    Viewing client receiving image data as string

--- ViewingClient/ViewingClient.py
+++ ViewingClient/ViewingClient.py
@@ -1,14 +1,44 @@
 from connection_service import ConnectionService
 
+import multiprocessing
+import cv2
+import time
+
+import numpy as np
+
 HOST = "127.0.0.1"
 PORT = 80
 VIEW_CLIENT =  f"client_type:1".encode()
 
 
+def display_frame(frame_data):
+    if frame_data is not None:
+        # Display the resulting frame
+        nparr = np.fromstring(frame_data, np.uint8)
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        try:
+            cv2.imshow('ViewingClient', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                raise SystemExit("Exiting...")
+        except Exception as e:
+            print ("Type error displaying image...")
+            print (e)
+            pass
+
+def display_frame_func(connection_service):
+    while True:
+        if not connection_service.is_connected():
+            pass
+        message = connection_service.get_message()
+        display_frame(message)
 
 def main():
     connection_service = ConnectionService(HOST, PORT, VIEW_CLIENT)
     connection_service.connect(VIEW_CLIENT)
+
+    display_frame_thread = multiprocessing.Process(target=display_frame_func, args=(connection_service,))
+    display_frame_thread.start()
 
                                             # Get available hubs / cameras
     # Request footage from hub + camera                                         # Set motion detect on hub + camera
